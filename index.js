45e7f419577809cba1f433f23e48d4c36e1dd25a
Andres Vargas <andres@python3.ninja>
2020-08-26 23:38:35 -0500

    update packages

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const ignore = require('ignore')
 const fs = require('fs')
 
-class ServerlessIgnore {
+class ServerlessSLSIgnore {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
+    this.pluginName = "slsignore";
 
     this.serverless.service.package = this.serverless.service.package || {}
 
@@ -29,7 +30,7 @@ class ServerlessIgnore {
       var ignoreFilePath = configFound ? config.file : '.slsignore'
       var ignoreFile = fs.readFileSync(ignoreFilePath, 'utf8').toString()
       var filesToIgnore = ignore().add(ignoreFile)._rules
-      this.serverless.cli.log('SERVERLESS-IGNORE: Loaded ' + ignoreFilePath)
+      this.serverless.cli.log(this.pluginName + ': Loaded ' + ignoreFilePath)
       
       // set exclude dev
       if (!configFound || (configFound && config.excludeDev == true)) this.serverless.service.package.excludeDevDependencies = true
@@ -37,16 +38,18 @@ class ServerlessIgnore {
       if (typeof this.serverless.service.package.exclude === 'undefined') {
         this.serverless.service.package.exclude = []
       }
+      let c =0;
       filesToIgnore.forEach((file) => {
         if (file.negative == true) return
         var fileIgnore = file.origin
-        this.serverless.cli.log("\t - " + fileIgnore);
         this.serverless.service.package.exclude.push(fileIgnore);
+        c+=1;
       })
+        this.serverless.cli.log(this.pluginName + ": files ignored: " + c);
     } catch (e) {
       console.error('\n Serverless Ignore Error --------------------------------------\n', '  ', e.message)
     }
   }
 }
 
-module.exports = ServerlessIgnore;
+module.exports = ServerlessSLSIgnore;
