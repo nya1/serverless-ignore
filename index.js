39535dccad9b0cc4df00d78fef08fbc2027c706a
nya1 <16636743+nya1@users.noreply.github.com>
2021-11-20 15:39:26 +0100

    refractor: use package.patterns instead of package.exclude
    ref https://www.serverless.com/framework/docs/deprecations#new-way-to-define-packaging-patterns

--- index.js
+++ index.js
@@ -7,41 +7,37 @@ class ServerlessIgnore {
     this.serverless = serverless;
     this.options = options;
 
+    // load service package object so we can edit it later
     this.serverless.service.package = this.serverless.service.package || {}
 
-    this.commands = {
-      deploy: {
-        lifecycleEvents: [
-          'package'
-        ]
-      },
-    };
-
     this.hooks = {
+      // bind hook with our own function
       'package:cleanup': this.ignoreFiles.bind(this)
     };
   }
 
   ignoreFiles() {
     try {
-      var config = (this.serverless.service.custom || {})['ignore'] || {};
-      var configFound = typeof config.file !== 'undefined'
-      var ignoreFilePath = configFound ? config.file : '.slsignore'
-      var ignoreFile = fs.readFileSync(ignoreFilePath, 'utf8').toString()
-      var filesToIgnore = ignore().add(ignoreFile)._rules
+      const config = (this.serverless.service.custom || {})['ignore'] || {};
+      const configFound = typeof config.file !== 'undefined'
+      const ignoreFilePath = configFound ? config.file : '.slsignore'
+      const ignoreFile = fs.readFileSync(ignoreFilePath, 'utf8').toString()
+      const filesToIgnore = ignore().add(ignoreFile)._rules
       this.serverless.cli.log('SERVERLESS-IGNORE: Loaded ' + ignoreFilePath)
       
       // set exclude dev
       if (!configFound || (configFound && config.excludeDev == true)) this.serverless.service.package.excludeDevDependencies = true
       
-      if (typeof this.serverless.service.package.exclude === 'undefined') {
-        this.serverless.service.package.exclude = []
+      // using package.patterns as suggested for 2+ sls versions
+      // ref https://www.serverless.com/framework/docs/deprecations#new-way-to-define-packaging-patterns
+      if (typeof this.serverless.service.package.patterns === 'undefined') {
+        this.serverless.service.package.patterns = []
       }
       filesToIgnore.forEach((file) => {
         if (file.negative == true) return
         var fileIgnore = file.origin
         this.serverless.cli.log("\t - " + fileIgnore);
-        this.serverless.service.package.exclude.push(fileIgnore);
+        this.serverless.service.package.patterns.push(fileIgnore);
       })
     } catch (e) {
       console.error('\n Serverless Ignore Error --------------------------------------\n', '  ', e.message)
